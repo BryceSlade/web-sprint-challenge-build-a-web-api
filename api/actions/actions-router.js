4c2aafa2bd56a83ebdd9e8e220e9826b9bcec0c5
Bryce Slade <bryceslade@gmail.com>
2021-12-03 10:12:03 -0700

    GET projects and actions tests passing

--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -1,1 +1,19 @@
-// Write your "actions" router here!
+const express = require('express');
+const Actions = require('./actions-model')
+
+const router = express.Router();
+
+router.get('/', (req, res) => {
+    Actions.get()
+        .then(actions => {
+            res.status(200).json(actions)
+        })
+        .catch(err => {
+            console.log(err)
+            res.status(400).json({
+                message: 'Error retrieving actions list'
+            })
+        })
+})
+
+module.exports = router;
--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -1,1 +1,19 @@
-// Write your "projects" router here!
+const express = require('express');
+const Projects = require('./projects-model')
+
+const router = express.Router();
+
+router.get('/', (req, res) => {
+    Projects.get()
+        .then(projects => {
+            res.status(200).json(projects)
+        })
+        .catch(err => {
+            console.log(err)
+            res.status(400).json({
+                message: 'Error retrieving projects list'
+            })
+        })
+})
+
+module.exports = router;
